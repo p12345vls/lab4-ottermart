ffa3596c3efd0b70d360076002ebf6f2934be5de
pavlos <p.pp256@yahoo.com>
2018-07-08 05:24:54 +0000

    modified to connect to heroku

--- dbConnection.php
+++ dbConnection.php
@@ -6,6 +6,16 @@ function getDatabaseConnection($dbname = 'ottermart') {
     $username = 'root';
     $password = '';
     
+     //when connecting from Heroku
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["bcc0fb604c5fe6"];
+        $password = $url["3451c4e0"];
+    } 
+
+    
     //creates new connection
     $dbConn = new PDO("mysql:host=$host; dbname=$dbname",$username,$password);
     //display errors when accessing tables

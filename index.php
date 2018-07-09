13aa2fe6e2e1e7275cecf49073232e4388008547
pavlos <p.pp256@yahoo.com>
2018-07-09 03:57:15 +0000

    updated files

--- index.php
+++ index.php
@@ -1,107 +1,131 @@
 <?php
+
 include 'dbConnection.php';
 
 $conn = getDatabaseConnection("ottermart");
 
 function displayCategories() {
     global $conn;
-    
+
     $sql = "Select catID, catName from om_category order by catName";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
+
     foreach ($records as $record) {
-        echo "<a href=\"purchaseHistory.php?productId=".$record["productId"]."\">History</a>";
-        echo $record["productName"]. " ". $record["productDescription"] ." $".$record["priceFrom"]."<br><br>";
-        echo "<option value ='".$record["catID"]."'>" .$record["catName"]."</option>";
+        echo $record["productName"] . " " . $record["productDescription"] . " $" . $record["priceFrom"] . "<br><br>";
+        echo "<option value ='" . $record["catID"] . "'>" . $record["catName"] . "</option>";
     }
 }
 
 function displaySearchResults() {
     global $conn;
-    
-    if(isset($_GET['searchForm'])) {
-        echo "<h3>Products Found: </h3>";
-        
+    echo "<div >";
+    if (isset($_GET['searchForm'])) {
+        echo "<h3 class='container'>Products Found </h3>";
+
         $namedParameters = array();
-        
+
         $sql = "SELECT * FROM om_product WHERE 1";
-               
-        if(!empty($_GET['product'])){
+
+        if (!empty($_GET['product'])) {
             $sql .= " AND productName LIKE :productName";
             $namedParameters[":productName"] = "%" . $_GET['product'] . "%";
         }
-        
-        if(!empty($_GET['category'])){
+
+        if (!empty($_GET['category'])) {
             $sql .= " AND catID = :categoryID";
-            $namedParameters[":categoryID"] =  $_GET['category'];
+            $namedParameters[":categoryID"] = $_GET['category'];
         }
-        
-        if(!empty($_GET['priceFrom'])){
+
+        if (!empty($_GET['priceFrom'])) {
             $sql .= " AND price >= :priceFrom";
-            $namedParameters[":priceFrom"] = $_GET['priceFrom'] ;
+            $namedParameters[":priceFrom"] = $_GET['priceFrom'];
         }
-        if(!empty($_GET['priceTo'])){
+        if (!empty($_GET['priceTo'])) {
             $sql .= " AND price <= :priceTo";
-            $namedParameters[":priceTo"] =  $_GET['priceTo'] ;
+            $namedParameters[":priceTo"] = $_GET['priceTo'];
         }
-        
-         if(!empty($_GET['orderBy'])) {
-              if(($_GET['order']=="price")) {
-                  $sql .= " ORDER BY price";
-              } else {
-                  $sql .= " ORDER BY productName";
-              }
-         }
-        
+
+        if (!empty($_GET['orderBy'])) {
+            if (($_GET['orderBy'] == "price")) {
+                $sql .= " ORDER BY price";
+            } else {
+                $sql .= " ORDER BY productName";
+            }
+        }
+
 
         $stmt = $conn->prepare($sql);
         $stmt->execute($namedParameters);
         $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        foreach ($records as $record) {
-            echo  $record["productName"] . " " . $record["productDescription"] . " $" . 
-            $record["price"] . "<br><br>";
-        } 
 
-        
+        foreach ($records as $record) {
+            
+            echo "<table class='container'>";
+            
+            echo "<tr><td><a href=\"purchaseHistory.php?productId=" . $record["productId"] . "\">History</a>&nbsp;&nbsp;&nbsp;";
+            echo $record["productName"] . " " . $record["productDescription"] . " $" .
+            $record["price"] . "</td></tr>";
+            
+            echo "</table>";
+        }
     }
+    echo "<div>";
 }
 
 ?>
-
+<!DOCTYPE html>
 <html>
     <head>
         <title>OtterMart Product Search</title>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="css/styles.css" type="text/css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/lab-5-ottermart/main.css" type="text/css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
-    <body>
-        <div>
-            <h1>OtterMart Product Search</h1>
-            
-            <form>
-                Product: <input type="text" name="product"/>
-            </br>
-            Category:
-            <select name="category">
-                <option value="">Select One</option>
-                <?=displayCategories()?>
-            </select>
-            </br>
-            Price: Form <input type="text" name="priceFrom" size="7"/>
-                    To  <input type="text" name="priceTo" size="7"/>
-                    <br>
-                    Order Result By
-                    <br>
-                    
-                    <input type="radio" name="orderBy" value="price"/>Price<br>
-                    <input type="radio" name="orderBy" value="name"/>Name<br>
-                    <br><br>
+    <body class="well" >
+        <div class="container">
+            <h2>OtterMart Product Search</h2>
 
-                <input type="submit" value="Search" name="searchForm"/>
+            <form>
+                <div class="form-group">
+                    <label for="usr">Name:</label>
+                    <input type="text" class="form-control" id="usr" name="product">
+                </div>
+                <div class="form-group">
+                    <label for="sel1">Category:</label>
+                    <select class="form-control" id="sel1" name="category">
+                        <option value="">Select One</option>
+                        <?=displayCategories()?>
+                    </select>
+                </div>
+
+                <label for="From">Price Range:</label>
+                <div class="form-check form-control-inline">
+
+                    <input type="text" class="form-control-input"
+                           id="From" placeholder=" From" name="priceFrom" size="7">
+
+                    &nbsp;&nbsp; 
+                    <input type="text" class="form-control-input"
+                           id="to" placeholder=" To" name="priceTo" size="7">
+                </div>
+                <hr>
+
+                <label>Order Results By:</label>
+                <div class="radio">
+                    <label><input type="radio" name="orderBy" value="price">Price</label>
+                </div>
+                <div class="radio">
+                    <label><input type="radio" name="orderBy" value="name">Name</label>
+                </div>
+                
+                <button type="submit" class="btn btn-primary" value="Search"
+                        name="searchForm">Search</button>
+                <hr>
             </form>
             </br>
         </div>
--- purchaseHistory.php
+++ purchaseHistory.php
@@ -1,32 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>OtterMart Product Search</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="/lab-5-ottermart/main.css" type="text/css" />
+
+    </head>
+</html>
+
 <?php
-    include '../dbConnection.php';
-    
-    echo "from purchaseHistory".$host;
-    
-    $conn = getDatabaseConnection("ottermart");
-    
-    $productId = $_GET['productId'];
-    
-    $sql = "SELECT * FROM om_product NATURAL JOIN om_purchase WHERE productId = :pId";
-            
-    $np = array();
-    $np[":pId"] = $productId;
-    
-    
-    $smt = $conn->prepare($sql);
-    $smt->execute($np);
-    $records = $smt->fetchAll(PDO::FETCH_ASSOC);
-    
-    echo $records[0]['productName'] . "<br/>";
-    echo "<img src='" . $records[0]['productImage'] . "' width='200'/><br/>";
-    
-    foreach($records as $record) {
-        
-        echo "Purchase Date: ". $record["purchaseDate"]."<br>";
-        echo "Unit price: ". $record["unitPrice"]."<br>";
-        echo "Quantity: ". $record["quantity"]."<br>";
-        
+include 'dbConnection.php';
+
+$conn = getDatabaseConnection("ottermart");
+
+$productId = $_GET['productId'];
+
+$sql = "SELECT * FROM om_product
+            NATURAL JOIN om_purchase
+            WHERE productId = :pId";
+
+$np = array();
+$np[":pId"] = $productId;
+
+
+$smt = $conn->prepare($sql);
+$smt->execute($np);
+$records = $smt->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+if ($records[0]['productName'] == '') {
+    echo "<div class='noRecords'>";
+     echo "<a href='index.php'><button>BACK</button></a>";
+    echo "<p >" . " &nbsp; &nbsp;&nbsp; &nbsp; No Records" . "</p>";
+   echo "<hr>";
+    echo "</div>";
+} else {
+
+    echo "<div class='history'>";
+    echo "<table >";
+
+    echo "<tr><th colspan='4'>" . $records[0]['productName'] . "</th></tr>";
+
+    foreach ($records as $record) {
+        echo "<td>" . "Purchase Date: " . $record["purchaseDate"] . "</td>";
+        echo "<td>" . "Unit price: " . $record["unitPrice"] . "</td>";
+        echo "<td>" . "Quantity: " . $record["quantity"] . "</td>";
     }
 
- 
+    echo "<td>" . "<img src='" . $records[0]['productImage'] . "' width=''/>" . "</td></tr>";
+
+
+    echo "</table>";
+
+    echo "</div>";
+}
 ?>
